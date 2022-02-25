3fb7919c825682f66204b29a33de0f5638a6f17a
ArakelianAra <100391549+ArakelianAra@users.noreply.github.com>
2022-02-24 20:52:48 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var box
+function setup() {
+  createCanvas(800, 500);
+  //box=createSprite(100,200,30,30)
+}
+
+function draw() {
+  background("black");
+  fill("blue")
+  rect(100,100,50,60)
+  //drawSprites()
+}
+
